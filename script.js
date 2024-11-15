32084bd443094fcda05ccd02ac6411075c6ad85f
Sam-Ola-24 <toye.akinola@gmail.com>
2024-11-15 01:45:31 -0400

    Added form validation

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const expand = document.getElementsByClassName("more");
-const less = document.getElementsByClassName("less");
+const submit = document.getElementById("submit-btn");
+
+submitError = "All fields are required *";
+submit.addEventListener("click", validateForm);
 
 let i;
 
 for (i = 0; i < expand.length; i++) {
-  expand[i].addEventListener("click", function() {
+  expand[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let details = this.nextElementSibling;
     if (details.style.display === "block") {
@@ -15,3 +18,14 @@ for (i = 0; i < expand.length; i++) {
   });
 }
 
+//To execute form submission
+function validateForm() {
+  if (
+    !document.getElementById("firstname").value ||
+    !document.getElementById("lastname").value ||
+    !document.getElementById("email").value ||
+    !document.getElementById("comment").value
+  ) {
+    document.getElementById("error").innerHTML = submitError;
+  }
+}
