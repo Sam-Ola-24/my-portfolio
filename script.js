3fdf31fa5f9502406d7eecaf3c99727221d144df
Sam-Ola-24 <toye.akinola@gmail.com>
2024-12-04 18:56:25 -0400

    Added minified versions of JS and CSS files, after testing site performance

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const expand = document.getElementsByClassName("more");
 const submit = document.getElementById("submit-btn");
-
-//----implemented the dark-mode button functionality here
-
 const toggleThemeButton = document.getElementById("toggleTheme");
 toggleThemeButton.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
-
-//----implemented the filter button functionality and added the option to clear filters
-
 const filterButtons = document.querySelectorAll(".filter-btn");
 const projects = document.querySelectorAll(".projects");
-
 filterButtons.forEach((button) => {
   button.addEventListener("click", function () {
     for (i = 0; i < filterButtons.length; i++) {
@@ -32,11 +25,7 @@ filterButtons.forEach((button) => {
     });
   });
 });
-
-//----implemented the read more button functionality here and text changes when in 'read-more' mode
-
 let i;
-
 for (i = 0; i < expand.length; i++) {
   expand[i].addEventListener("click", function () {
     this.classList.toggle("active");
@@ -50,12 +39,8 @@ for (i = 0; i < expand.length; i++) {
     }
   });
 }
-
-//To execute form validation
-
 submitError = "All fields are required *";
 submit.addEventListener("click", validateForm);
-
 function validateForm() {
   if (
     !document.getElementById("firstname").value ||
@@ -66,83 +51,57 @@ function validateForm() {
     document.getElementById("error").innerHTML = submitError;
   }
 }
-
-//-----implemented canvas images for html page
-
 function firstImage() {
   const ctx = document.getElementById("canvas").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/portfolio.png";
 }
-
 firstImage();
-
 function secondImage() {
   const ctx = document.getElementById("css").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/css.png";
 }
-
 secondImage();
-
 function thirdImage() {
   const ctx = document.getElementById("blog").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/webStorage.png";
 }
-
 thirdImage();
-
 function fourthImage() {
   const ctx = document.getElementById("lab").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/lab.png";
 }
-
 fourthImage();
-
 function fifthImage() {
   const ctx = document.getElementById("port").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/pportfolio.png";
 }
-
 fifthImage();
-
 function sixthImage() {
   const ctx = document.getElementById("assign").getContext("2d");
   const img = new Image();
-
   img.addEventListener("load", () => {
     ctx.drawImage(img, 0, 0, 300, 150);
   });
-
   img.src = "./images/code.png";
 }
-
 sixthImage();
